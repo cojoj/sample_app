a2d32b712b50e9eb0dda0ea0787a2ce57eee3729
Mateusz Zając <matthewhare91@gmail.com>
2013-08-14 22:53:08 +0200

    Uniqueness of emails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130813211658) do
+ActiveRecord::Schema.define(version: 20130814204541) do
 
   create_table "users", force: true do |t|
     t.string   "name"
