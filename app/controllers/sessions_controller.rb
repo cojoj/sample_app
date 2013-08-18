65ec8328708e5f43a9038327fec67333bdf7b2cb
cojoj <matthewhare91@gmail.com>
2013-08-18 23:02:43 +0200

    Changing the layout links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,12 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-      if user && user.authenticate(params[:session][:password])
-        # Sign the user in and redirect to the user's show page.
-      else
-        flash.now[:error] = 'Invalid email/password combination'
-        render 'new'
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
     end
   end
   
