91f3ddcea3e30cd27b85e8b7173f9b5ed5634829
cojoj <matthewhare91@gmail.com>
2013-08-20 22:40:32 +0200

    Requiring the right user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -10,18 +11,17 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or user
     else
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
   
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
@@ -37,14 +37,23 @@ class UsersController < ApplicationController
   
   private
 
-      def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
-      end
-      
-      # Before filters
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 
-      def signed_in_user
-        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
       end
-  
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
 end
