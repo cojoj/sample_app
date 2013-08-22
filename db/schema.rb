8ea7cbb38013b86df2b08bf0661f0cd1e37879c4
Mateusz Zając <matthewhare91@gmail.com>
2013-08-22 22:01:51 +0200

    Pagination

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20130818202215) do
     t.string   "remember_token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
